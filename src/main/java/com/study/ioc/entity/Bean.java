73dd9d3f22f9d5968f64990ac29d2cd417f8c621
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-10-27 17:46:51 +0300

    add lombok

--- src/main/java/com/study/ioc/entity/Bean.java
+++ src/main/java/com/study/ioc/entity/Bean.java
@@ -1,27 +1,15 @@
 package com.study.ioc.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bean {
     private String id;
     private Object value;
-
-    public Bean(String id, Object value) {
-        this.id = id;
-        this.value = value;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Object getValue() {
-        return value;
-    }
-
-    public void setValue(Object value) {
-        this.value = value;
-    }
 }
