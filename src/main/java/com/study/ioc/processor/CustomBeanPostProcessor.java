9d60fd0b8bfd69b60fd2e64d4de4086c47488072
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-10-30 10:53:16 +0200

    refactor GenericApplicationContextITest

--- src/main/java/com/study/ioc/processor/CustomBeanPostProcessor.java
+++ src/main/java/com/study/ioc/processor/CustomBeanPostProcessor.java
@@ -5,13 +5,11 @@ import com.study.ioc.entity.Bean;
 public class CustomBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Bean postProcessBeforeInitialization(String beanName, Bean bean) {
-        bean.setId("mailServiceIMAP");
         return bean;
     }
 
     @Override
     public Bean postProcessAfterInitialization(String beanName, Bean bean) {
-        bean.setId("mailServiceIMAP");
         return bean;
     }
 }
