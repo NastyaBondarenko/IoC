a030db4c0cb9a2a1086b5ac633f43b100a70c19a
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-10-30 13:19:29 +0200

    refactor CustomBeanPostProcessor

--- src/main/java/com/study/ioc/processor/CustomBeanPostProcessor.java
+++ src/main/java/com/study/ioc/processor/CustomBeanPostProcessor.java
@@ -5,11 +5,17 @@ import com.study.ioc.entity.Bean;
 public class CustomBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Bean postProcessBeforeInitialization(String beanName, Bean bean) {
+        if (bean.getId().equals("userServiceImap")) {
+            bean.setId("userService");
+        }
         return bean;
     }
 
     @Override
     public Bean postProcessAfterInitialization(String beanName, Bean bean) {
+        if (bean.getId().equals("userServiceImap")) {
+            bean.setId("userService");
+        }
         return bean;
     }
 }
