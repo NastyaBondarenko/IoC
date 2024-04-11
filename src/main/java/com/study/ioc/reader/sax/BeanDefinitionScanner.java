61b02db80a675624ad62b297f93414e9289a994d
NastyaBondarenko <av_bondarenko@rambler.ru>
2024-04-12 00:46:46 +0300

    Refactor BeanDefinitionScanner

--- src/main/java/com/study/ioc/reader/sax/BeanDefinitionScanner.java
+++ src/main/java/com/study/ioc/reader/sax/BeanDefinitionScanner.java
@@ -4,7 +4,7 @@ import com.study.ioc.entity.BeanDefinition;
 import com.study.ioc.reader.BeanDefinitionReader;
 import lombok.AllArgsConstructor;
 import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.Scanners;
 import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
 
@@ -22,7 +22,7 @@ public class BeanDefinitionScanner implements BeanDefinitionReader {
                 .flatMap(packageName -> new Reflections(new ConfigurationBuilder()
                         .forPackages(packageName)
                         .filterInputsBy(new FilterBuilder().includePackage(packageName))
-                        .setScanners(new SubTypesScanner(false)))
+                        .setScanners(Scanners.SubTypes.filterResultsBy(s -> true)))
                         .getSubTypesOf(Object.class).stream()
                         .map(clazz -> new BeanDefinition(clazz.getSimpleName(), clazz.getName())))
                 .collect(Collectors.toMap(BeanDefinition::getId, beanDefinition -> beanDefinition));
