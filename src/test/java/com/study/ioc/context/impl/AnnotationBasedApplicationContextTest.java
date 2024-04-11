a557f84b61f4eaf01e2127cc0e1a31ed0b4e82ae
NastyaBondarenko <av_bondarenko@rambler.ru>
2024-04-12 00:47:53 +0300

    Refactor AnnotationBasedApplicationContextTest=

--- src/test/java/com/study/ioc/context/impl/AnnotationBasedApplicationContextTest.java
+++ src/test/java/com/study/ioc/context/impl/AnnotationBasedApplicationContextTest.java
@@ -147,39 +147,39 @@ public class AnnotationBasedApplicationContextTest {
         assertTrue(expectedBeansNames.containsAll(actualBeansNames));
     }
 
-//    @Test
-//    @DisplayName("should inject value dependencies successfully")
-//    public void shouldInjectValueDependenciesSuccessfully() {
-//        Map<String, Bean> beanMap = new HashMap<>();
-//        Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
-//
-//        MailService mailServicePOP = new MailService();
-//        beanMap.put("mailServicePOP", new Bean("mailServicePOP", mailServicePOP));
-//        MailService mailServiceIMAP = new MailService();
-//        beanMap.put("mailServiceIMAP", new Bean("mailServiceIMAP", mailServiceIMAP));
-//
-//        //  setPort(110) and setProtocol("POP3") via valueDependencies
-//        BeanDefinition popServiceBeanDefinition = new BeanDefinition("mailServicePOP", "com.study.testclasses.MailService");
-//        Map<String, String> popServiceValueDependencies = new HashMap<>();
-//        popServiceValueDependencies.put("port", "110");
-//        popServiceValueDependencies.put("protocol", "POP3");
-//        popServiceBeanDefinition.setValueDependencies(popServiceValueDependencies);
-//        beanDefinitionMap.put("mailServicePOP", popServiceBeanDefinition);
-//
-//        //  setPort(143) and setProtocol("IMAP") via valueDependencies
-//        BeanDefinition imapServiceBeanDefinition = new BeanDefinition("mailServiceIMAP", "com.study.testclasses.MailService");
-//        Map<String, String> imapServiceValueDependencies = new HashMap<>();
-//        imapServiceValueDependencies.put("port", "143");
-//        imapServiceValueDependencies.put("protocol", "IMAP");
-//        imapServiceBeanDefinition.setValueDependencies(imapServiceValueDependencies);
-//        beanDefinitionMap.put("mailServiceIMAP", imapServiceBeanDefinition);
-//
-//        annotationBasedApplicationContext.injectValueDependencies(beanDefinitionMap, beanMap);
-//        assertEquals(110, mailServicePOP.getPort());
-//        assertEquals("POP3", mailServicePOP.getProtocol());
-//        assertEquals(143, mailServiceIMAP.getPort());
-//        assertEquals("IMAP", mailServiceIMAP.getProtocol());
-//    }
+    @Test
+    @DisplayName("should inject value dependencies successfully")
+    public void shouldInjectValueDependenciesSuccessfully() {
+        Map<String, Bean> beanMap = new HashMap<>();
+        Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
+
+        MailService mailServicePOP = new MailService();
+        beanMap.put("mailServicePOP", new Bean("mailServicePOP", mailServicePOP));
+        MailService mailServiceIMAP = new MailService();
+        beanMap.put("mailServiceIMAP", new Bean("mailServiceIMAP", mailServiceIMAP));
+
+        //  setPort(110) and setProtocol("POP3") via valueDependencies
+        BeanDefinition popServiceBeanDefinition = new BeanDefinition("mailServicePOP", "com.study.testclasses.MailService");
+        Map<String, String> popServiceValueDependencies = new HashMap<>();
+        popServiceValueDependencies.put("port", "110");
+        popServiceValueDependencies.put("protocol", "POP3");
+        popServiceBeanDefinition.setValueDependencies(popServiceValueDependencies);
+        beanDefinitionMap.put("mailServicePOP", popServiceBeanDefinition);
+
+        //  setPort(143) and setProtocol("IMAP") via valueDependencies
+        BeanDefinition imapServiceBeanDefinition = new BeanDefinition("mailServiceIMAP", "com.study.testclasses.MailService");
+        Map<String, String> imapServiceValueDependencies = new HashMap<>();
+        imapServiceValueDependencies.put("port", "143");
+        imapServiceValueDependencies.put("protocol", "IMAP");
+        imapServiceBeanDefinition.setValueDependencies(imapServiceValueDependencies);
+        beanDefinitionMap.put("mailServiceIMAP", imapServiceBeanDefinition);
+
+        annotationBasedApplicationContext.injectValueDependencies(beanDefinitionMap, beanMap);
+        assertEquals(110, mailServicePOP.getPort());
+        assertEquals("POP3", mailServicePOP.getProtocol());
+        assertEquals(143, mailServiceIMAP.getPort());
+        assertEquals("IMAP", mailServiceIMAP.getProtocol());
+    }
 
 //    @Test
 //    public void testInjectRefDependencies() {
