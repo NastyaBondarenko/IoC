b4936b41590da37574b4d671031e65578ef9620a
NastyaBondarenko <av_bondarenko@rambler.ru>
2024-06-07 00:00:19 +0300

    Refactor AutowiredBeanPostProcessor:
    - refactor postProcessBeforeInitialization;

--- src/main/java/com/bondarenko/ioc/processor/AutowiredBeanPostProcessor.java
+++ src/main/java/com/bondarenko/ioc/processor/AutowiredBeanPostProcessor.java
@@ -2,10 +2,14 @@ package com.bondarenko.ioc.processor;
 
 import com.bondarenko.ioc.annotation.Autowired;
 import com.bondarenko.ioc.annotation.Order;
+import com.bondarenko.ioc.exception.NoUniqueBeanOfTypeException;
 import lombok.SneakyThrows;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.*;
+
+import static java.lang.String.format;
 
 
 @Order(Integer.MIN_VALUE)
@@ -13,18 +17,30 @@ public class AutowiredBeanPostProcessor implements BeanPostProcessor {
 
     private final Map<Class<?>, List<Object>> groupedBeansByClass = new HashMap<>();
 
-
     @Override
     public Object postProcessBeforeInitialization(String beanName, Object beanValue) {
         Arrays.stream(beanValue.getClass().getDeclaredFields())
                 .filter(field -> field.isAnnotationPresent(Autowired.class))
                 .forEach(field -> {
-                    Class<?> type = field.getType();
-                    groupedBeansByClass.getOrDefault(type, Collections.emptyList()).stream()
-                            .findFirst()
-                            .ifPresent(value -> findMethodToInjectRefDependencies(beanValue, field.getName(), value));
+                    checkForMultipleBeans(field.getType(), beanName);
+                    processFieldInjection(beanValue, field);
                 });
         return beanValue;
+    }
+
+    private void checkForMultipleBeans(Class<?> type, String beanName) {
+        Optional.ofNullable(groupedBeansByClass.get(type))
+                .filter(list -> list.size() > 1)
+                .ifPresent(list -> {
+                    throw new NoUniqueBeanOfTypeException(format("Multiple beans of type %s in %s", type.getName(), beanName));
+                });
+    }
+
+    private void processFieldInjection(Object beanValue, Field field) {
+        groupedBeansByClass.getOrDefault(field.getType(), Collections.emptyList())
+                .stream()
+                .findFirst()
+                .ifPresent(value -> findMethodToInjectRefDependencies(beanValue, field.getName(), value));
     }
 
     @Override
